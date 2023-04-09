f7eb62afd3763975b032a1c0195b8fed6743015b
breakidawn <breakidawn@qq.com>
2023-04-09 10:59:14 +0800

    更新url

--- MMSLS.js
+++ MMSLS.js
@@ -19,18 +19,18 @@ const MMSLS = [
 =====================请在下面编辑=====================
  */
 const MMSLS = [
-    "https://www.maimemo.com/share/page?uid=25379641&pid=1502cecb7c6bdededaa2e0982eaa0f7b&tid=15a0e16dcc0107798fd6b2aee9ce1105",
-  ]
-  /**
-   * 生成随机数字
-   * @param {number} min 最小值（包含）
-   * @param {number} max 最大值（不包含）
-   */
-  function randomNumber(min = 0, max = 100) {
-    return Math.min(Math.floor(min + Math.random() * (max - min)), max);
-  }
-  const MMSL_random = MMSLS[randomNumber(0, MMSLS.length)];
-  
-  module.exports = {
-    MMSL_random
-  }
+  ""
+]
+/**
+ * 生成随机数字
+ * @param {number} min 最小值（包含）
+ * @param {number} max 最大值（不包含）
+ */
+function randomNumber(min = 0, max = 100) {
+  return Math.min(Math.floor(min + Math.random() * (max - min)), max);
+}
+const MMSL_random = MMSLS[randomNumber(0, MMSLS.length)];
+
+module.exports = {
+  MMSL_random
+}

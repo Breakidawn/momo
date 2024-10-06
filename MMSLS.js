58e9650fc8c79011136598b2971c3681cbef6d2b
WenBin <85985300+Breakidawn@users.noreply.github.com>
2024-10-06 11:58:14 +0800

    Update MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -1,6 +1,6 @@
 
 const MMSLS = [
-"https://www.maimemo.com/share/page?uid=25379641&pid=74e923de8a2d9dd688adbff3dbdec360&tid=d66f5462dc0cd4c6b676dc7ab5334ae5"
+"https://www.maimemo.com/share/page?uid=25379641&pid=5afeb4e40c10c56d1ebd872af8369a82&tid=6473580e5b937d2c961091a5a84b2325"
 ]
 /**
  * 生成随机数字
